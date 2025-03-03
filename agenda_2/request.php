f4f822da8f38770712d52f01cf262b2eb8d35aa0
Paulo Henrique <127267437+AshSlake@users.noreply.github.com>
2025-03-02 21:04:17 -0300

    metodo get e post PHP

---
+++ agenda_2/request.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>GET e POST</title>
+  </head>
+  <body>
+    <form id="form1" name="form1" method="post" action="acao.php">
+      <br />
+      <input name="nome" type="text" id="nome" placeholder="digitar o nome" />
+      <input name="enviar" type="submit" id="enviar" value="enviar" />
+    </form>
+  </body>
+</html>
